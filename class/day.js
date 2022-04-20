ef48ba250ce59e1f7c09d0a5bda209b94fe88a79
YEBBLEE <leeyeb9458@gmail.com>
2022-04-20 16:52:27 +0900

    챌린지 오브젝트 생성시 필요한 day 데이터 만들기 따로 분리함

---
+++ class/day.js
@@ -0,0 +1,16 @@
+export class Day {
+    constructor(number, isChecked) {
+        this.number = number;
+        this.isChecked = isChecked;
+    }
+}
+
+export function initDays() {
+    let days = [];
+
+    for (let i = 0; i < 30; i++) {
+        const day = new Day(i+1, false);
+        days.push(day);
+    }
+    return days;
+}
