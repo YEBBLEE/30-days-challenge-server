42b05b6105a0a9845d165bbc690c2d0cf6e0ef59
YEBBLEE <leeyeb9458@gmail.com>
2022-04-20 16:51:25 +0900

    startDate, endDate 표기함수 분리

---
+++ common/date.js
@@ -0,0 +1,13 @@
+export function setStartDate() {
+    let startStr = new Date().toLocaleDateString();
+    return startStr.substring(0, startStr.lastIndexOf('.'));
+}
+
+export function setEndDate() {
+    let date = new Date();
+    date.setDate(date.getDate() + 30);
+    let endStr = date.toLocaleDateString();
+    return endStr.substring(0, endStr.lastIndexOf('.'));
+}
+
+
