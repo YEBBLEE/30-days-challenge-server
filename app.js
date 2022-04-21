d83e350359384527edbcd3c2f45b8a322fc52c2a
YEBBLEE <leeyeb9458@gmail.com>
2022-04-21 16:41:25 +0900

    cors 모듈 추가

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 import express from 'express';
+import cors from 'cors';
 import challengesRouter from './router/challenges.js';
 
 
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 
 console.log('app!');
 
