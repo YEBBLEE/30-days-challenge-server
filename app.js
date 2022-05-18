2135df5f71007bb65ef0d476682ce74306984e69
YEBBLEE <leeyeb9458@gmail.com>
2022-05-18 17:50:00 +0900

    ssl적용된 서버에 https로 응답 보내기위해 https모듈 설치 & app.js 코드 https로 수정함.

--- app.js
+++ app.js
@@ -4,6 +4,14 @@ import challengesRouter from './router/challenges.js';
 import authRouter from './router/auth.js';
 import { config } from './config.js';
 import { db } from './db/database.js';
+import https from 'https';
+import fs from 'fs';
+
+const hostname = 'yebb-whybbb.com';
+var privateKey = fs.readFileSync(`/etc/letsencrypt/live/${hostname}/privkey.pem`);
+var certificate = fs.readFileSync(`/etc/letsencrypt/live/${hostname}/cert.pem`);
+var ca = fs.readFileSync(`/etc/letsencrypt/live/${hostname}/chain.pem`);
+const credentials = { key: privateKey, cert: certificate, ca: ca };
 
 const app = express();
 
@@ -23,4 +31,5 @@ app.use((error,req,res,next) => {
 });
 
 db.getConnection().then((connection)=> console.log(connection));
-app.listen(config.host.port);
+
+https.createServer(credentials, app).listen(config.host.port);
